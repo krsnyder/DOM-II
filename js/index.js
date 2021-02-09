6c98dcbc26683efe0c3a681753a6dc11946f8414
Kirk <kirkrobertsnyder@gmail.com>
2021-02-09 16:33:53 -0500

    Popped the balloon

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
-// Adding an alert upon load
+// Adding an alert upon load 
 // window.addEventListener('load', () => {
 //   alert("Hello! Lots of fun stuff here, don't get too excited by all the changing colors!")
 // })
 
 // Scrolling through an array of classes for the background colors
+
+let click = document.getElementById('click');
 let bodyColor = document.querySelector('body');
 let colors = ['black', 'blue', 'red', 'green', 'white']
-bodyColor.addEventListener('click', () => {
+click.addEventListener('click', () => {
   let index = colors.indexOf(bodyColor.classList[0]);
   if (index == 4) {
     bodyColor.classList.remove('white');
@@ -18,11 +20,24 @@ bodyColor.addEventListener('click', () => {
 })
 
 // Scroll event
-window.addEventListener('scroll', () => {
-  const scrollable = document.documentElement.scrollHeight - window.innerHeight;
-  const scrolled = window.scrollY;
+// window.addEventListener('scroll', () => {
+//   const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+//   const scrolled = window.scrollY;
 
-  if (scrolled == scrollable) {
-    alert('You have reached the bottom of the page')
-  }
+//   if (scrolled == scrollable) {
+//     alert('You have reached the bottom of the page')
+//   }
+// })
+
+
+// Balloon pop
+const balloon = document.getElementById('balloon');
+const pop = document.getElementById('pop');
+balloon.addEventListener("mouseover", () => {
+  alert("You popped my balloon!")
+  balloon.classList.add("off")
+  pop.classList.remove("off")
 })
+
+
+

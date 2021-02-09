7f4e3dc7a607156ca243b249df8a464f41ce8f37
Kirk <kirkrobertsnyder@gmail.com>
2021-02-09 14:23:29 -0500

    Added function for changing background color

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+let bodyColor = document.querySelector('body');
+let colors = ['black', 'blue', 'red', 'green', 'white']
+bodyColor.addEventListener('click', () => {
+  let index = colors.indexOf(bodyColor.classList[0]);
+
+  if (index == 4) {
+    bodyColor.classList.remove('white');
+    bodyColor.classList.add('black');
+  } else {
+    bodyColor.classList.remove(colors[index]);
+    bodyColor.classList.add(colors[index+1]);
+  }
+})

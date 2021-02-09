1f56523eda79799b58a015153f1a99e6b9470b9a
Kirk <kirkrobertsnyder@gmail.com>
2021-02-09 17:59:19 -0500

    Added form

--- js/index.js
+++ js/index.js
@@ -9,7 +9,8 @@ let click = document.getElementById('click');
 let pic = document.getElementsByTagName('img');
 let pics = Array.prototype.slice.call(pic);
 
-click.addEventListener('click', () => {
+click.addEventListener('click', (e) => {
+  e.stopPropagation();
   for (let i = 0; i < pics.length; i++){
     pics[i].classList.toggle('off')
   }
@@ -20,43 +21,64 @@ click.addEventListener('click', () => {
   }
 })
 
-
-
-
-
 // Scrolling through an array of classes for the background colors
 
-// let bodyColor = document.querySelector('body');
-// let colors = ['black', 'blue', 'red', 'green', 'white']
-// bodyColor.addEventListener('click', () => {
-//   let index = colors.indexOf(bodyColor.classList[0]);
-//   if (index == 4) {
-//     bodyColor.classList.remove('white');
-//     bodyColor.classList.add('black');
-//   } else {
-//     bodyColor.classList.remove(colors[index]);
-//     bodyColor.classList.add(colors[index+1]);
-//   }
-// })
+let bodyColor = document.querySelector('body');
+let colors = ['black', 'blue', 'red', 'green', 'white']
+bodyColor.addEventListener('click', () => {
+  let index = colors.indexOf(bodyColor.classList[0]);
+  if (index == 4) {
+    bodyColor.classList.remove('white');
+    bodyColor.classList.add('black');
+  } else {
+    bodyColor.classList.remove(colors[index]);
+    bodyColor.classList.add(colors[index+1]);
+  }
+})
 
 // Scroll event
 
-// window.addEventListener('scroll', () => {
-//   const scrollable = document.documentElement.scrollHeight - window.innerHeight;
-//   const scrolled = window.scrollY;
+window.addEventListener('scroll', () => {
+  const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+  const scrolled = window.scrollY;
 
-//   if (scrolled == scrollable) {
-//     alert('You have reached the bottom of the page')
-//   }
-// })
-
+  if (scrolled == scrollable) {
+    alert('You have reached the bottom of the page')
+  }
+})
 
 // Balloon pop
 
-// const balloon = document.getElementById('balloon');
-// const pop = document.getElementById('pop');
-// balloon.addEventListener("mouseover", () => {
-//   alert("You popped my balloon!")
-//   balloon.classList.add("off")
-//   pop.classList.remove("off")
+const balloon = document.getElementById('balloon');
+const pop = document.getElementById('pop');
+balloon.addEventListener("mouseover", () => {
+  alert("You popped my balloon!")
+  balloon.classList.add("off")
+  pop.classList.remove("off")
+})
+
+// Prevent Reload
+
+// const anchor = document.getElementById('anchor');
+// anchor.addEventListener('click', (e) => {
+//   e.preventDefault()
 // })
+
+// Form
+
+let form = document.getElementById('form')
+form.addEventListener('click', e => {
+  e.stopPropagation();
+})
+
+// const password = document.querySelector('input[type="password"]');
+
+form.addEventListener('focus', (e) => {
+  e.target.style.background = 'gray';
+  e.target.style.color = 'white'
+}, true);
+
+form.addEventListener('blur', (e) => {
+  e.target.style.background = 'white';
+  e.target.style.color = 'black'
+}, true);
